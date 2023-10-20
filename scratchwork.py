f626d6d1509271c9673de4635990d6912e0e76a3
pete rodrigue <edwardpierrerodrigue@gmail.com>
2023-10-20 18:05:37 -0400

    Update scratchwork.py

--- scratchwork.py
+++ scratchwork.py
@@ -1,23 +1,38 @@
 import requests
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import math
+import re
+import pyttsx3
+engine = pyttsx3.init()
 
-headers = {
-    'accept': '*/*',
-    'x-api-key': EB_KEY,
-}
 
-response = requests.get('https://syndication.api.eb.com/production/article/39330/xml', headers=headers)
 
-response_xml_as_string = response.text.encode('utf-8')
-print(response_xml_as_string)
-asXML = ET.fromstring(response_xml_as_string)
+soup = BeautifulSoup(requests.get("https://factanimal.com/elephant/").content, "html.parser")
 
-rv = ''
-for p in asXML.findall(".//p"):
-    # Get all inner text
-    rv = rv + "".join(t.encode('utf-8') for t in p.itertext())
+rv = []
+counter = 0
+list_index = 0
+for el in soup.find_all('p'):
+    next_string = el.text
+    next_string = next_string.replace('Fact Animal', '')
+    next_string = next_string.replace('Facts About Animals', '')
+    if counter == 0:
+        rv.append(next_string)
+        counter += 1
+    elif counter == 6:
+        rv[list_index] += ' '
+        rv[list_index] += next_string
+        list_index += 1
+        counter = 0
+    else:
+        rv[list_index] += next_string
+        counter += 1
 
-rv = rv.split(",",1)[1]
 
-converter.say(rv)
-converter.runAndWait()
+
+print(rv)
+print(rv[0])
+
+engine.say(rv[0])
+engine.runAndWait()
+
